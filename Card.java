64295cd223d008ee244a9a580985ec01932a3c4c
Aadityaramrame <aadityaramrame@gmail.com>
2025-03-05 22:17:57 +0530

    Uploading the Card file

---
+++ Card.java
@@ -0,0 +1,13 @@
+
+class Card {
+    String rank;
+    String suit;
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
